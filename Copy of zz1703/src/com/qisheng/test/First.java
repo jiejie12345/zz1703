f0ef9fa0cbcbe926c145906ffca8e8e2bca5e54a
Administrator <Administrator@USER-20170516UM>
2017-08-21 22:27:05 +0800

    main()

---
+++ Copy of zz1703/src/com/qisheng/test/First.java
@@ -0,0 +1,27 @@
+package com.qisheng.test;
+
+public class First {
+	
+	
+	
+	
+	
+	
+	
+	
+	private void mian() {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello");
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
