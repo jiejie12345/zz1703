9979126edb7576ef9e24bf53ade83d3ee0ba00c5
Administrator <Administrator@USER-20170516UM>
2017-08-21 20:34:25 +0800

    新建项目

--- superGit/src/com/qisheng/Test.java
+++ superGit/src/com/qisheng/Test.java
@@ -2,17 +2,11 @@ package com.qisheng;
 
 public class Test {
 	
-	
-	private static int i = 10;
-	
-	
 	public static void main(String[] args) {
-		
-		System.out.println(i+1);
-		
 		
 		System.out.println("hello world!");
 		
 	}
+	
 
 }
